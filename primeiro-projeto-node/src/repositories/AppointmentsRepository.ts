b816aeff574b84c16979bad813bf2292b76cbbbd
Caio Ceretta <caioceretta@gmail.com>
2020-09-22 22:59:42 -0300

    Primeiro Node

--- primeiro-projeto-node/src/repositories/AppointmentsRepository.ts
+++ primeiro-projeto-node/src/repositories/AppointmentsRepository.ts
@@ -1,15 +1,32 @@
+import { isEqual } from 'date-fns';
 import Appointment from '../models/Appointment';
 
-class AppointmentsRepository {
+interface createAppointmentDTO {
+  provider: string;
+  date: Date;
+}
 
+class AppointmentsRepository {
   private appointments: Appointment[];
 
   constructor() {
     this.appointments = [];
   }
 
-  public create(provider: string, date: Date): Appointment {
-    const appointment = new Appointment(provider, date);
+  public all(): Appointment[] {
+    return this.appointments;
+  }
+
+  public findByDate(date: Date): Appointment | null {
+    const findAppointmentInSameDate = this.appointments.find(appointment =>
+      isEqual(date, appointment.date),
+    );
+
+    return findAppointmentInSameDate || null;
+  }
+
+  public create({ provider, date }: createAppointmentDTO): Appointment {
+    const appointment = new Appointment({ provider, date });
 
     this.appointments.push(appointment);
 

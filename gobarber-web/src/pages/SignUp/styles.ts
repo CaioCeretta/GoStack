2aaf4b834955bb54bc6019864b7f55779bf91740
Caio Ceretta <caioceretta@gmail.com>
2022-11-27 22:48:18 -0300

    Created the signup page

---
+++ gobarber-web/src/pages/SignUp/styles.ts
@@ -0,0 +1,92 @@
+import styled from 'styled-components';
+import { shade } from 'polished';
+
+import SignUpBackground from '../../assets/sign-up-background.png';
+
+export const Container = styled.div`
+  height: 100vh;
+
+  display: flex;
+  align-items: stretch;
+`;
+
+export const Content = styled.div`
+  display: flex;
+  flex-direction: column;
+  /* justify-content: center;
+  align-items: center;*/
+
+  align-items: center;
+
+  place-content: center; // Faz as duas coisas acima em apenas um comando
+
+  width: 100%;
+  max-width: 700px;
+
+  form {
+    width: 340px;
+    margin: 80px 0;
+    text-align: center;
+
+    h1 {
+      margin-bottom: 24px;
+    }
+
+    input {
+      background: #232129;
+      border-radius: 10px;
+      border: 2px solid #232129;
+      padding: 16px;
+      width: 100%;
+      color: #f4ede8;
+
+      &::placeholder {
+        color: #666360;
+      }
+
+      & + input {
+        margin-top: 8px;
+      }
+    }
+
+    a {
+      color: #f4ede8;
+      display: block;
+
+      margin-top: 24px;
+      text-decoration: none;
+
+      transition: color 0.2s;
+
+      &:hover {
+        color: ${shade(0.2, '#F4EDE8')};
+      }
+    }
+  }
+
+  > a {
+    color: #f4ede8;
+    display: block;
+    margin-top: 24px;
+    text-decoration: none;
+
+    display: flex;
+    align-items: center;
+
+    transition: color 0.2s;
+
+    svg {
+      margin-right: 16px;
+    }
+
+    &:hover {
+      color: ${shade(0.2, '#f4ede8')};
+    }
+  }
+`;
+
+export const Background = styled.div`
+  flex: 1;
+  background: url(${SignUpBackground}) no-repeat center;
+  background-size: cover;
+`;

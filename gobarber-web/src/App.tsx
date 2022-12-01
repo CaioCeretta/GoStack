96debff1a80cf47d5911dd56bd2b1ace5a2c80bf
Caio Ceretta <caioceretta@gmail.com>
2022-12-01 10:52:31 -0300

    Created the context and the authentication of the app

--- gobarber-web/src/App.tsx
+++ gobarber-web/src/App.tsx
@@ -4,10 +4,14 @@ import SignUp from './pages/SignUp';
 // import SignIn from './pages/SignIn';
 import GlobalStyle from './styles/global';
 
+import { AuthProvider } from './hooks/AuthContext';
+
 const App: React.FC = () => (
   <>
     <GlobalStyle />
-    <SignIn />
+    <AuthProvider>
+      <SignIn />
+    </AuthProvider>
   </>
 );
 
--- gobarber-web/src/pages/SignIn/index.tsx
+++ gobarber-web/src/pages/SignIn/index.tsx
@@ -1,44 +1,56 @@
 import React, { useCallback, useRef } from 'react';
 import * as Yup from 'yup';
-
 import { FiLogIn, FiMail, FiLock } from 'react-icons/fi';
 import { Form } from '@unform/web';
-
 import { FormHandles } from '@unform/core';
+
+import { useAuth } from '../../hooks/AuthContext';
+import getValidationErrors from '../../utils/getValidationErrors';
+
 import logoImg from '../../assets/logo.svg';
 
 import Input from '../../components/Input';
 import Button from '../../components/Button';
 
 import { Container, Content, Background } from './styles';
-import getValidationErrors from '../../utils/getValidationErrors';
+
+interface SignInFormData {
+  email: string;
+  password: string;
+}
 
 const SignIn: React.FC = () => {
   const formRef = useRef<FormHandles>(null);
 
-  const handleSubmit = useCallback(async (data: object) => {
-    try {
-      formRef.current?.setErrors({});
+  const { signIn } = useAuth();
 
-      const schema = Yup.object().shape({
-        name: Yup.string().required('The name is required'),
-        email: Yup.string()
-          .email('Type a valid e-mail address')
-          .required('The email is required'),
-        password: Yup.string().min(6, 'At least 6 characters'),
-      });
+  const handleSubmit = useCallback(
+    async (data: SignInFormData) => {
+      try {
+        formRef.current?.setErrors({});
 
-      await schema.validate(data, {
-        abortEarly: false,
-      });
-    } catch (err: any) {
-      console.log(err);
+        const schema = Yup.object().shape({
+          email: Yup.string()
+            .email('Type a valid e-mail address')
+            .required('The email is required'),
+          password: Yup.string().min(6, 'At least 6 characters'),
+        });
 
-      const errors = getValidationErrors(err);
+        await schema.validate(data, {
+          abortEarly: false,
+        });
 
-      formRef.current?.setErrors(errors);
-    }
-  }, []);
+        signIn({ email: data.email, password: data.password });
+      } catch (err: any) {
+        console.log(err);
+
+        const errors = getValidationErrors(err);
+
+        formRef.current?.setErrors(errors);
+      }
+    },
+    [signIn],
+  );
 
   return (
     <Container>

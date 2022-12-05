efba38dab163fc384ee3460e90c65f0fe52df669
Caio Ceretta <caioceretta@gmail.com>
2022-12-04 22:11:51 -0300

    Finished the GoBarber web

--- gobarber-web/src/pages/SignUp/index.tsx
+++ gobarber-web/src/pages/SignUp/index.tsx
@@ -3,8 +3,12 @@ import { FiMail, FiLock, FiUser, FiArrowLeft } from 'react-icons/fi';
 import { FormHandles } from '@unform/core';
 import { Form } from '@unform/web';
 import * as Yup from 'yup';
-import { Link } from 'react-router-dom';
+import { Link, useHistory } from 'react-router-dom';
+import api from '../../services/api';
+
 import getValidationErrors from '../../utils/getValidationErrors';
+
+import { useToast } from '../../hooks/toast';
 
 import logoImg from '../../assets/logo.svg';
 
@@ -13,32 +17,64 @@ import Button from '../../components/Button';
 
 import { Container, Content, Background, AnimationContainer } from './styles';
 
+interface SignUpFormData {
+  name: string;
+  email: string;
+  password: string;
+}
+
 const SignUp: React.FC = () => {
   const formRef = useRef<FormHandles>(null);
+  const { addToast } = useToast();
+  const history = useHistory();
 
-  const handleSubmit = useCallback(async (data: object) => {
-    try {
-      formRef.current?.setErrors({});
+  const handleSubmit = useCallback(
+    async (data: object) => {
+      try {
+        formRef.current?.setErrors({});
 
-      const schema = Yup.object().shape({
-        name: Yup.string().required('The name is required'),
-        email: Yup.string()
-          .email('Type a valid e-mail address')
-          .required('The email is required'),
-        password: Yup.string().min(6, 'At least 6 characters'),
-      });
+        const schema = Yup.object().shape({
+          name: Yup.string().required('The name is required'),
+          email: Yup.string()
+            .email('Type a valid e-mail address')
+            .required('The email is required'),
+          password: Yup.string().min(6, 'At least 6 characters'),
+        });
 
-      await schema.validate(data, {
-        abortEarly: false,
-      });
-    } catch (err: any) {
-      console.log(err);
+        await schema.validate(data, {
+          abortEarly: false,
+        });
 
-      const errors = getValidationErrors(err);
+        await api.post('/users', data);
 
-      formRef.current?.setErrors(errors);
-    }
-  }, []);
+        addToast({
+          type: 'success',
+          title: 'Register succeded',
+          description: 'You can sign in into GoBarber',
+        });
+
+        history.push('/');
+      } catch (err) {
+        if (err instanceof Yup.ValidationError) {
+          const errors = getValidationErrors(err);
+
+          formRef.current?.setErrors(errors);
+
+          return;
+        }
+
+        // trigger a toast
+
+        addToast({
+          type: 'error',
+          title: 'Register error',
+          description:
+            'An error has occurred creating your register, try again',
+        });
+      }
+    },
+    [addToast, history],
+  );
 
   return (
     <Container>
@@ -57,7 +93,7 @@ const SignUp: React.FC = () => {
             <Input
               name="password"
               icon={FiLock}
-              type="text"
+              type="password"
               placeholder="Password"
             />
 
